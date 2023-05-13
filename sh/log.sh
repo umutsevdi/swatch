dd1783cf001f5d6f37ee4e574432d586efa0f533
Umut Sevdi <sevdiumut@gmail.com>
2023-05-13 13:48:52 +0300

    improved log text

--- sh/log.sh
+++ sh/log.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/usr/bin/bash
 #******************************************************************************
 #
 # * File: nc.sh
@@ -9,9 +9,8 @@
 #*****************************************************************************
 
 function Help {
-   echo "loglist"
-   echo "  A helpful script that prints out formatted logs of given application"
-   echo
+   echo "loglist - A helpful script that prints out formatted logs of given"
+   echo "application"
    echo "Syntax: [-h/c|m]"
    echo
    echo "Options:"
@@ -22,13 +21,32 @@ function Help {
 }
 
 function nextcloud {
-    docker logs app_nc_1  | grep GET | \
-        awk '{printf "%s%s - %s %s %s::%s\n", $4,$5, $1, $3, $12, $7}'
+    docker logs app_nc_1  -n 100 2>/dev/null | \
+        awk '{printf "%s %s %s\t%s\t%s\n", $1,$9, $3,$12, $7}' | \
+        grep -E "[0-9\. ]+ [A-Za-z]+"
 }
+
+#******************************************************************************
+#                               www.service
+#******************************************************************************
 
 function site {
     journalctl -u www.service --no-pager | grep main | \
         awk '{printf "[%s %s] %s %s %s\t%s\t%s\n",$6,$7,$8,$12,$13,$14,$15}'
+}
+
+# returns ip addresses of recent clients
+function ip {
+    journalctl -o cat -u www.service --no-pager | awk '{print $7}' | \
+        grep "[0-9.]" | uniq | tail
+}
+
+function pages {
+    journalctl -o cat -u www.service --no-pager | grep "\[main" | \
+        awk '{printf "%s %s %s %s\n",$7, $8, $9,$10}' | sed "s/serveStatic# /\//g" | \
+        sed "s/serve#/\//g" | tr -d "]" | sed -E "s/ ([0-9]{3})/\t\1/g" | \
+        sed -E "s/\/ ([A-Za-z]*)/\/\1/g" | \
+        sed -E "s/([0-9.]+) ([^\0]+)\t([0-9]+)/\1\t\3\t\2/g" | uniq | tail
 }
 
 
@@ -40,7 +58,7 @@ for arg in $@; do
         nextcloud
         exit
     elif [ $arg = -s ] || [ $arg = --site ]; then
-        site
+        pages
         exit
     else
         echo -e "Error: Invalid arguments" 1>&2
